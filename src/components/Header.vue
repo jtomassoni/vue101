036ddca9d57a0e206d42d386634941b11a63867f
James Tomassoni <james.tomassoni@fareharbor.com>
2023-01-05 22:15:09 -0700

    App works with fake backend json-server

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+  <ButtonNew
+    @btn-click="$emit('toggle-add-task')"
+    :text="showAddTask ? 'Close' : 'Add Task'"
+    :color="showAddTask ? 'red' : 'green'" />
+</template>
+
+<script>
+import ButtonNew from './Button.vue'
+
+export default {
+  name: 'HeaderNew',
+  props: {
+    title: {
+      type: String,
+      //Will appear if nothing is passed
+      //when HeaderNew is called in App.vue
+      default: 'Hello World'
+    },
+    showAddTask: Boolean
+  },
+  components: {
+    ButtonNew
+  },
+  emits: ['toggle-add-task']
+  }
+</script>
+
+
+<style scope>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <!-- <h3>{{ task.text }}</h3> -->
+    <TaskNew
+      @toggle-reminder="$emit('toggle-reminder',task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task" />
+  </div>
+  
+</template>
+
+<script>
+import TaskNew from './Task.vue'
+
+export default {
+  name: 'TasksNew',
+  props: {
+    tasks: Array
+  },
+  components: {
+    TaskNew
+  },
+  emits: ['delete-task', 'toggle-reminder']
+}
+</script>
